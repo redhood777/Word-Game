ac6cd2300e4c54379327e1d8f9cc532afb8102ca
Apurva <paiapurva273@gmail.com>
2021-08-05 09:08:10 +0530

    Lvl2 90% done

--- Assets/Scripts/ScrambleScentence.cs
+++ Assets/Scripts/ScrambleScentence.cs
@@ -5,20 +5,42 @@ using UnityEngine.UI;
 
 public class ScrambleScentence : MonoBehaviour
 {
-    [HideInInspector]
-    string sentence = "This is a scentence";
-    string[] words;
-    public GameObject optionsPanel;
-    public Button buttonPrefab;
-    public Text buttonText;
-    
+    public static ScrambleScentence instance;
 
+    [HideInInspector]
+    string sentence;
+    public string answerSentence;
+    string[] words;
+    string[] jumbledWords;
+    List<string> sentences;
+    public GameObject optionsPanel;
+    public GameObject answerPanel;
+    public GameObject buttonPrefab;
+   // public GameObject resetButton;
+   // public GameObject submitButton;
+    public Text buttonText;
+    [SerializeField]
+    private SentenceDataScriptable sentenceDataScriptable;
+    public GameObject correctAnswerText;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            GameObject.Destroy(this);
+    }
     // Start is called before the first frame update
     void Start()
     {
+        sentences = sentenceDataScriptable.sentences;
+        sentence = sentences[Random.Range(0, sentences.Count)];
+        answerSentence = sentence + " ";
+        Debug.Log(answerSentence);
         buttonText = buttonPrefab.GetComponentInChildren<Text>();
         SplitSentence(sentence);
-        InstantiateButton();
+        RandomizeArray(words);
+        
         
     }
 
@@ -32,19 +54,74 @@ public class ScrambleScentence : MonoBehaviour
     {
         string str = sentence;
         words = sentence.Split(' ');
-        //Debug.Log(words.Length);
-        for (int i = 0; i < words.Length; i++)
+        ////Debug.Log(words.Length);
+        //for (int i = 0; i < words.Length; i++)
+        //{
+        //    Debug.Log(words[i]);
+        //}
+    }
+
+    public void RandomizeArray(string[] sentenceArray)
+    {
+        for(int i = sentenceArray.Length - 1; i > 0; i--)
         {
-            Debug.Log(words[i]);
+            var r = Random.Range(0, i);
+            var temp = sentenceArray[i];
+            sentenceArray[i] = sentenceArray[r];
+            sentenceArray[r] = temp;
+        }
+
+        for (int i = 0; i < sentenceArray.Length; i++)
+        {
+            GameObject wordButton = Instantiate(buttonPrefab, optionsPanel.transform);
+            wordButton.GetComponentInChildren<Text>().text = sentenceArray[i];
+        }
+
+        optionsPanel.GetComponent<VerticalLayoutGroup>().childScaleHeight = false;
+
+    }
+
+    public void OptionSelected(GameObject option)
+    {
+        GameObject copy;
+        copy = Instantiate(option);
+        copy.transform.SetParent(answerPanel.transform);
+        option.SetActive(false);
+
+    }
+
+    public void ResetOptions()
+    {
+        foreach(Transform child in answerPanel.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach(Transform child in optionsPanel.transform)
+        {
+            child.gameObject.SetActive(true);
         }
     }
 
-    public void InstantiateButton()
+    public void SubmitAnswer()
     {
-        for(int i = 0; i < words.Length; i++)
+        string answer = string.Empty;
+        foreach (Transform child in answerPanel.transform)
         {
-            Button wordButton = Instantiate(buttonPrefab, optionsPanel.transform);
-            wordButton.GetComponentInChildren<Text>().text = words[i];
+            string tmp = child.gameObject.GetComponentInChildren<Text>().text + " ";
+            answer = answer + tmp;
+            tmp = string.Empty;
+        }
+        Debug.Log(answerSentence);
+        Debug.Log(answer);
+
+        if (answer == answerSentence)
+        {
+            correctAnswerText.GetComponent<Text>().text = "Correct Answer";
+        }
+        else
+        {
+            correctAnswerText.GetComponent<Text>().text = "Wrong Answer";
         }
     }
 
