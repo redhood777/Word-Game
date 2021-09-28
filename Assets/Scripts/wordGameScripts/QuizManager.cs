3872a89a3f89efe7642294da53bf96312338f66e
shubham20t <agrawalshubhamop@gmail.com>
2021-09-28 10:24:39 +0530

    1.0
    101

--- Assets/Scripts/wordGameScripts/QuizManager.cs
+++ Assets/Scripts/wordGameScripts/QuizManager.cs
@@ -126,7 +126,12 @@ public class QuizManager : MonoBehaviour
         greenImage.enabled = false;
 
         //set the answerWord string variable
+<<<<<<< HEAD
         answerWord = questionDataScriptable.questions[num].answer;
+=======
+        
+        answerWord = questionDataScriptable.questions[go].answer;
+>>>>>>> ea3d6eddd5ecc2a29f77d9dcca7903cb9d065a7c
         Debug.Log(answerWord);
         //set the image of question
         questionImage.sprite = questionDataScriptable.questions[num].questionImage;
